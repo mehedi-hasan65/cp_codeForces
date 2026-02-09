32cacf7a6c1c082f56ec7a9c6ceec99c88f68b1a
Mehedi Hasan <mehedihasan634st@gmail.com>
2026-02-09 13:19:03 +0600

    =W_W

---
+++ codeForce1/temprdawej.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        int x = n - (k - 1);
+        int y = n - 2 * (k - 1);
+        if (x >= 1 && x % 2 == 1)
+        {
+            cout << "YES" << endl;
+            for (int i = 1; i <= k - 1; i++)
+            {
+                cout << "1" << " ";
+            }
+            cout << x << endl;
+        }
+        if (y >= 1 && y % 2 == 0)
+        {
+            cout << "YES" << endl;
+            for (int i = 1; i <= (k - 1); i++)
+            {
+                cout << "2" << " ";
+            }
+            cout << y << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
